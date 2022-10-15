dec429540f0ea612bfa777517deeb5a814b7ab4f
Vint_Razor <vint-razor@yandex.ru>
2022-10-15 23:58:40 +0500

    2.1.1. Парсинг https://career.habr.com/vacancies/java_developer?page=1 [#285210 #351842]

--- src/main/java/ru/job4j/quartz/HabrCareerParse.java
+++ src/main/java/ru/job4j/quartz/HabrCareerParse.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 public class HabrCareerParse {
     private static final String SOURCE_LINK = "http://career.habr.com";
     private static final String PAGE_LINK = String.format("%s/vacancies/java_developer", SOURCE_LINK);
+    private static final int END_PAGE = 5;
 
     public static void main(String[] args) throws IOException {
         Connection connection = Jsoup.connect(PAGE_LINK);
@@ -22,15 +23,15 @@ public class HabrCareerParse {
         System.out.println(retrieveDescription(link));
     }
 
-    private static void pageTurner(int endPage) throws IOException {
-        for (int i = 1; i <= endPage; i++) {
+    private static void pageTurner() throws IOException {
+        for (int i = 1; i <= END_PAGE; i++) {
             String url = String.format("%s%s%d", PAGE_LINK, "?page=", i);
             parse(url);
         }
     }
 
     private static void parse(String url) throws IOException {
-        Connection connection = Jsoup.connect(PAGE_LINK);
+        Connection connection = Jsoup.connect(url);
         Document document = connection.get();
         Elements rows = document.select(".vacancy-card__inner");
         rows.forEach(row -> {

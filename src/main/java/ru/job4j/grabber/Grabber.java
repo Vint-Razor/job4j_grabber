0ec95eac82cb7d43ac74d4da1ab1f32a6ac1584c
Рустам <vint-razor@yandex.ru>
2022-11-15 15:40:56 +0500

    7. Web. [#289478]

--- src/main/java/ru/job4j/grabber/Grabber.java
+++ src/main/java/ru/job4j/grabber/Grabber.java
@@ -1,11 +1,17 @@
 package ru.job4j.grabber;
 
 
+import org.slf4j.Logger;
 import org.quartz.*;
 import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.Charset;
 import java.util.Properties;
 
 import static org.quartz.JobBuilder.newJob;
@@ -13,9 +19,9 @@ import static org.quartz.SimpleScheduleBuilder.simpleSchedule;
 import static org.quartz.TriggerBuilder.newTrigger;
 
 public class Grabber implements Grab {
-
     private final Properties cfg = new Properties();
     private static final String HABR = "http://career.habr.com/vacancies/java_developer";
+    private static final Logger LOG = LoggerFactory.getLogger(Grabber.class.getName());
 
     public Store store() {
         return new PsqlStore(cfg);
@@ -60,7 +66,31 @@ public class Grabber implements Grab {
             Store store = (Store) map.get("store");
             Parse parse = (Parse) map.get("parse");
             parse.list(HABR).forEach(store::save);
+            LOG.info("saving to the database is completed");
         }
+    }
+
+    public void web(Store store) {
+        new Thread(() -> {
+            String port = cfg.getProperty("port");
+            try (ServerSocket server = new ServerSocket(Integer.parseInt(port))) {
+                while (!server.isClosed()) {
+                    Socket socket = server.accept();
+                    try (OutputStream out = socket.getOutputStream()) {
+                        out.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
+                        for (Post post : store.getAll()) {
+                            out.write(post.toString().getBytes(Charset.forName("Windows-1251")));
+                            out.write(System.lineSeparator().getBytes());
+                        }
+                        LOG.info("the request has been sent port: {}", port);
+                    } catch (IOException io) {
+                        io.printStackTrace();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }).start();
     }
 
     public static void main(String[] args) throws Exception {
@@ -69,5 +99,6 @@ public class Grabber implements Grab {
         Scheduler scheduler = grab.scheduler();
         Store store = grab.store();
         grab.init(new HabrCareerParse(new HabrCareerDateTimeParser()), store, scheduler);
+        grab.web(store);
     }
 }
